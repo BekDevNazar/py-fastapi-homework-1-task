fa0dab7186a09eb1c99fda633a2e0ab02f9facb4
Nazar <hehelele281@gmail.com>
2026-09-23 10:02:10 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,75 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas import MovieListResponseSchema, MovieDetailResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movie_with_pagination(
+        request: Request,
+        page: int = Query(1, ge=1),
+        per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db)
+):
+    offset = (page - 1) * per_page
+
+    result = await db.execute(
+        select(MovieModel)
+        .offset(offset)
+        .limit(per_page)
+    )
+
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(
+            status_code=404,
+            detail="No movies found."
+        )
+
+    total_items = await db.scalar(
+        select(func.count(MovieModel.id))
+    )
+
+    total_pages = (
+        total_items + per_page - 1
+    ) // per_page
+
+    prev_page = None
+    if page > 1:
+        prev_page = (
+            f"{request.url.path}"
+            f"?page={page - 1}&per_page={per_page}"
+        )
+
+    next_page = None
+
+    if page < total_pages:
+        next_page = (
+            f"{request.url.path}"
+            f"?page={page + 1}&per_page={per_page}"
+        )
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items,
+    }
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie_by_id(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+    if movie is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+    return movie
